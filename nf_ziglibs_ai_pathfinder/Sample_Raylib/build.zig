7068e2da99488d603e87573c572f6bb3e1acbfde
netpyoung <netpyoung@gmail.com>
2026-08-07 20:54:45 +0900

    update: nf_ziglibs_ai_pathfinder/Sample_Raylib/build.zig
    - but not build now...
      - ref: https://github.com/raylib-zig/raylib-zig/issues/343

--- nf_ziglibs_ai_pathfinder/Sample_Raylib/build.zig
+++ nf_ziglibs_ai_pathfinder/Sample_Raylib/build.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
+const rlz = @import("raylib_zig");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -11,34 +12,34 @@ pub fn build(b: *std.Build) void {
 
     const mod_pathfinder = dep_pathfinder.module("nf_ziglibs_ai_pathfinder");
 
-    const exe = b.addExecutable(.{
-        .name = "Sample_Raylib",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{
-                .{ .name = "nf_ziglibs_ai_pathfinder", .module = mod_pathfinder },
-            },
-        }),
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+        .imports = &.{
+            .{ .name = "nf_ziglibs_ai_pathfinder", .module = mod_pathfinder },
+        },
     });
 
-    {
-        // setup raylib
-        // ref: https://github.com/raylib-zig/raylib-zig
+    const exe = b.addExecutable(.{
+        .name = "Sample_Raylib",
+        .root_module = exe_mod,
+    });
 
-        const raylib_dep = b.dependency("raylib_zig", .{
-            .target = target,
-            .optimize = optimize,
-        });
+    // setup raylib
+    // ref: https://github.com/raylib-zig/raylib-zig
 
-        const raylib = raylib_dep.module("raylib"); // main raylib module
-        const raygui = raylib_dep.module("raygui"); // raygui module
-        const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
-        exe.root_module.linkLibrary(raylib_artifact);
-        exe.root_module.addImport("raylib", raylib);
-        exe.root_module.addImport("raygui", raygui);
-    }
+    const raylib_dep = b.dependency("raylib_zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib"); // main raylib module
+    const raygui = raylib_dep.module("raygui"); // raygui module
+    const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
+    exe.root_module.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
 
     b.installArtifact(exe);
 
@@ -49,5 +50,35 @@ pub fn build(b: *std.Build) void {
 
     if (b.args) |args| {
         run_cmd.addArgs(args);
+    }
+
+    if (target.query.os_tag == .emscripten) {
+        const emsdk = rlz.emsdk;
+        const wasm = b.addLibrary(.{
+            .name = "HelloWeb",
+            .root_module = exe_mod,
+        });
+
+        const install_dir: std.Build.InstallDir = .{ .custom = "web" };
+        const emcc_flags = emsdk.emccDefaultFlags(b.allocator, .{ .optimize = optimize });
+        const emcc_settings = emsdk.emccDefaultSettings(b.allocator, .{ .optimize = optimize });
+
+        const emcc_step = emsdk.emccStep(b, raylib_artifact, wasm, .{
+            .optimize = optimize,
+            .flags = emcc_flags,
+            .settings = emcc_settings,
+            .install_dir = install_dir,
+        });
+        b.getInstallStep().dependOn(emcc_step);
+
+        const html_filename = try std.fmt.allocPrint(b.allocator, "{s}.html", .{wasm.name});
+        const emrun_step = emsdk.emrunStep(
+            b,
+            b.getInstallPath(install_dir, html_filename),
+            &.{},
+        );
+
+        emrun_step.dependOn(emcc_step);
+        run_step.dependOn(emrun_step);
     }
 }
