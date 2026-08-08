5e91e4d7d9db937a459988b9a0ba2281a4f2add2
netpyoung <netpyoung@gmail.com>
2026-08-09 00:31:21 +0900

    update: build.zig - zig build all ( windows / linux / macos / ios / tvos / android / wasm )

--- nf_ziglibs_ai_pathfinder/nf_ziglibs_ai_pathfinder/build.zig
+++ nf_ziglibs_ai_pathfinder/nf_ziglibs_ai_pathfinder/build.zig
@@ -1,53 +1,139 @@
 ﻿const std = @import("std");
 
+const PlatformOption = struct {
+    step_name: []const u8,
+    targets: []const std.Build.ResolvedTarget,
+    linkage: std.builtin.LinkMode,
+    optimize: std.builtin.OptimizeMode,
+};
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const root_module = b.addModule("nf_ziglibs_ai_pathfinder", .{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-        .strip = (optimize != .Debug),
-    });
+    const platformOptions: []const PlatformOption = &.{
+        .{
+            .step_name = "windows",
+            .targets = &.{
+                b.resolveTargetQuery(.{
+                    .os_tag = .windows,
+                    .cpu_arch = .x86_64,
+                }),
+                b.resolveTargetQuery(.{
+                    .os_tag = .windows,
+                    .cpu_arch = .aarch64,
+                }),
+            },
+            .linkage = .dynamic,
+            .optimize = optimize,
+        },
+        .{
+            .step_name = "linux",
+            .targets = &.{
+                b.resolveTargetQuery(.{
+                    .os_tag = .linux,
+                    .cpu_arch = .x86_64,
+                }),
+                b.resolveTargetQuery(.{
+                    .os_tag = .linux,
+                    .cpu_arch = .aarch64,
+                }),
+            },
+            .linkage = .dynamic,
+            .optimize = optimize,
+        },
+        .{
+            .step_name = "macos",
+            .targets = &.{
+                b.resolveTargetQuery(.{
+                    .os_tag = .macos,
+                    .cpu_arch = .aarch64,
+                    .os_version_min = .{ .semver = .{ .major = 13, .minor = 0, .patch = 0 } },
+                }),
+            },
+            .linkage = .dynamic,
+            .optimize = optimize,
+        },
+        .{
+            .step_name = "ios",
+            .targets = &.{
+                b.resolveTargetQuery(.{
+                    .os_tag = .ios,
+                    .cpu_arch = .aarch64,
+                    .abi = null,
+                    .os_version_min = .{ .semver = .{ .major = 12, .minor = 0, .patch = 0 } },
+                }),
+                b.resolveTargetQuery(.{
+                    .os_tag = .ios,
+                    .cpu_arch = .aarch64,
+                    .abi = .simulator,
+                    .os_version_min = .{ .semver = .{ .major = 12, .minor = 0, .patch = 0 } },
+                }),
+            },
+            .linkage = .static,
+            .optimize = optimize,
+        },
+        .{
+            .step_name = "tvos",
+            .targets = &.{
+                b.resolveTargetQuery(.{
+                    .os_tag = .tvos,
+                    .cpu_arch = .aarch64,
+                    .os_version_min = .{ .semver = .{ .major = 13, .minor = 0, .patch = 0 } },
+                }),
+                b.resolveTargetQuery(.{
+                    .os_tag = .tvos,
+                    .cpu_arch = .aarch64,
+                    .abi = .simulator,
+                    .os_version_min = .{ .semver = .{ .major = 13, .minor = 0, .patch = 0 } },
+                }),
+            },
+            .linkage = .static,
+            .optimize = optimize,
+        },
+        .{
+            .step_name = "android",
+            .targets = &.{
+                b.resolveTargetQuery(.{
+                    .os_tag = .linux,
+                    .cpu_arch = .aarch64,
+                    .abi = .android,
+                    .os_version_min = .{ .semver = .{ .major = 24, .minor = 0, .patch = 0 } },
+                }),
+            },
+            .linkage = .dynamic,
+            .optimize = optimize,
+        },
+        .{
+            .step_name = "wasm",
+            .targets = &.{
+                b.resolveTargetQuery(.{
+                    .os_tag = .freestanding,
+                    .cpu_arch = .wasm32,
+                    .abi = .none,
+                }),
+            },
+            .linkage = .static,
+            .optimize = optimize,
+        },
+    };
 
-    const isDynamic = b.option(bool, "dynamic", "dynamic") orelse false;
+    const all_step = b.step("all", "Build libraries for all target platforms");
+    for (platformOptions) |platformOption| {
+        const step = AddPlatformStep(b, platformOption);
+        all_step.dependOn(step);
+    }
 
-    const lib = b.addLibrary(.{
-        .name = "nf_ziglibs_ai_pathfinder",
-        .linkage = if (isDynamic) .dynamic else .static,
+    // ===========================================
+    // for test
+    // ===========================================
+    const mod_tests = b.addTest(.{
         .root_module = b.createModule(.{
-            .root_source_file = b.path("src//exports.zig"),
+            .root_source_file = b.path("src/root.zig"),
             .target = target,
             .optimize = optimize,
             .strip = (optimize != .Debug),
         }),
-    });
-
-    lib.rdynamic = true;
-    lib.entry = .disabled;
-    
-    b.installArtifact(lib);
-
-    // _ = b.standardTargetOptions(.{});
-    // _ = b.standardOptimizeOption(.{});
-    //
-    // const step = b.step("task", "do something");
-    // step.makeFn = myTask;
-    // b.default_step = step;
-
-//    target.result.os.tag == .ios
-//    if (target.result.cpu.arch == .aarch64)
-
-//    const custom_path = b.fmt("a/b/{s}", .{lib.out_lib_filename});
-//    const custom_install = b.addInstallFile(
-//        lib.getEmittedBin(),
-//        custom_path,
-//    );
-//    b.getInstallStep().dependOn(&custom_install.step);
-
-    const mod_tests = b.addTest(.{
-        .root_module = root_module,
     });
 
     const run_mod_tests = b.addRunArtifact(mod_tests);
@@ -56,43 +142,55 @@ pub fn build(b: *std.Build) void {
     test_step.dependOn(&run_mod_tests.step);
 }
 
-fn myTask(step: *std.Build.Step, options: std.Build.Step.MakeOptions) !void {
-    std.debug.print("Hello!\n", .{});
-    _ = options;
-    _ = step;
-}
+fn AddPlatformStep(b: *std.Build, opt: PlatformOption) *std.Build.Step {
+    const step_desc = b.fmt("Build library for {s}", .{opt.step_name});
+    const ret = b.step(opt.step_name, step_desc);
 
-// fn Hello(step: *std.Build.Step, options: std.Build.Step.MakeOptions) !void {
-//     _ = options;
-//
-//     const target = b.standardTargetOptions(.{});
-//     const optimize = b.standardOptimizeOption(.{});
-//
-//     const root_module = b.createModule(.{
-//         .root_source_file = b.path("src/main.zig"),
-//         .target = target,
-//         .optimize = optimize,
-//     });
-//
-//     const lib = b.addLibrary(.{
-//         .name = "nf_ziglibs_ai_pathfinder",
-//         .root_module = root_module,
-//         .linkage = .dynamic,
-//     });
-//     //
-//     // // _ = lib.getEmittedH();
-//     b.installArtifact(lib);
-//     // const install_lib = b.addInstallArtifact(lib, .{ .h_dir = .{ .override = .header } });
-//     // install_lib.emitted_h = lib.getEmittedH();
-//     // b.getInstallStep().dependOn(&install_lib.step);
-//     const copy_to_a = b.option(bool, "a", "Copy DLL to ../a directory") orelse false;
-//
-//     if (copy_to_a) {
-//         const copy_file = b.addInstallFile(
-//             lib.getEmittedBin(),
-//             "../../a/nf_ziglibs_ai_pathfinder.dll",
-//         );
-//
-//         b.getInstallStep().dependOn(&copy_file.step);
-//     }
-// }
+    for (opt.targets) |target| {
+        const lib = b.addLibrary(.{
+            .name = "nf_ziglibs_ai_pathfinder",
+            .linkage = opt.linkage,
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("src/exports.zig"),
+                .target = target,
+                .optimize = opt.optimize,
+                .strip = (opt.optimize != .Debug),
+            }),
+        });
+
+        lib.rdynamic = true;
+        lib.entry = .disabled;
+
+        const folder_name = if (target.result.abi == .simulator)
+            b.fmt("{s}_{s}_simulator", .{
+                @tagName(target.result.os.tag),
+                @tagName(target.result.cpu.arch),
+            })
+        else if (target.result.abi == .android)
+            b.fmt("android_{s}", .{
+                @tagName(target.result.cpu.arch),
+            })
+        else
+            b.fmt("{s}_{s}", .{
+                @tagName(target.result.os.tag),
+                @tagName(target.result.cpu.arch),
+            });
+
+        const is_windows = target.result.os.tag == .windows;
+        if (is_windows) {
+            const install = b.addInstallArtifact(lib, .{
+                .dest_dir = .{ .override = .{ .custom = folder_name } },
+                .implib_dir = .{ .override = .{ .custom = folder_name } },
+                .pdb_dir = if (opt.optimize == .Debug) .{ .override = .{ .custom = folder_name } } else .default,
+            });
+            ret.dependOn(&install.step);
+        } else {
+            const install = b.addInstallArtifact(lib, .{
+                .dest_dir = .{ .override = .{ .custom = folder_name } },
+            });
+            ret.dependOn(&install.step);
+        }
+    }
+
+    return ret;
+}
