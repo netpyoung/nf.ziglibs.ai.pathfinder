317cd0cfbc2fa19fa42a5c365bf1f9521d006050
netpyoung <netpyoung@gmail.com>
2026-08-14 13:52:00 +0900

    change: DllImport to LibraryImport

--- interop/interop-dotnet/NF.Dotnetlibs.AI.Pathfinder/Library.cs
+++ interop/interop-dotnet/NF.Dotnetlibs.AI.Pathfinder/Library.cs
@@ -3,7 +3,7 @@
 namespace NF.Dotnetlibs.AI.Pathfinder;
 
 [StructLayout(LayoutKind.Sequential)]
-public struct int2
+public struct int2 : IEquatable<int2>
 {
     public int x;
     public int y;
@@ -22,6 +22,21 @@ public struct int2
     public static bool operator !=(int2 left, int2 right)
     {
         return !(left == right);
+    }
+
+    public bool Equals(int2 other)
+    {
+        return x == other.x && y == other.y;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is int2 other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(x, y);
     }
 }
 
@@ -57,7 +72,7 @@ public enum E_ERRORCODE : int
     ERR_PATHFINDER_NOT_ENOUGH_OUTBUF_SIZE = -4204,
 };
 
-public static class Library
+public static partial class Library
 {
 #if UNITY_EDITOR
     const string DLL_NAME = "nf_ziglibs_ai_pathfinder";
@@ -79,61 +94,68 @@ public static class Library
     // pf_debug_allocator_
     // =========================================
 
-    [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-    public static extern E_ERRORCODE pf_debug_allocator_init();
+    [LibraryImport(DLLNAME)]
+    // NOTE(pyoung): Omitted UnmanagedCallConv as it is only relevant on x86.
+    // ref: https://learn.microsoft.com/en-us/dotnet/standard/native-interop/calling-conventions
+    // [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
+    public static partial E_ERRORCODE pf_debug_allocator_init();
 
-    [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-    public static extern E_ERRORCODE pf_debug_allocator_deinit();
+    [LibraryImport(DLLNAME)]
+    public static partial E_ERRORCODE pf_debug_allocator_deinit();
 
     // =========================================
     // pf_jpsb_map_
     // =========================================
-    [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-    public static extern E_ERRORCODE pf_jpsb_map_create(int width, int height, out HandleJpsbMap outHandleMap);
+    [LibraryImport(DLLNAME)]
+    public static partial E_ERRORCODE pf_jpsb_map_create(int width, int height, out HandleJpsbMap outHandleMap);
 
-    [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-    public static extern E_ERRORCODE pf_jpsb_map_destroy(IntPtr ptr);
+    [LibraryImport(DLLNAME)]
+    public static partial E_ERRORCODE pf_jpsb_map_destroy(IntPtr ptr);
 
-    [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void pf_jpsb_map_set_wall_at(HandleJpsbMap handleMap, int x, int y, bool isWall);
+    [LibraryImport(DLLNAME)]
+    public static partial void pf_jpsb_map_set_wall_at(HandleJpsbMap handleMap, int x, int y, [MarshalAs(UnmanagedType.U1)] bool isWall);
 
-    [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-    public static extern void pf_jpsb_map_set_empty_at(HandleJpsbMap handleMap, int x, int y, bool isEmpty);
+    [LibraryImport(DLLNAME)]
+    public static partial void pf_jpsb_map_set_empty_at(HandleJpsbMap handleMap, int x, int y, [MarshalAs(UnmanagedType.U1)] bool isEmpty);
 
-    [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-    public static extern bool pf_jpsb_map_is_wall_at(HandleJpsbMap handleMap, int x, int y);
+    [LibraryImport(DLLNAME)]
+    [return: MarshalAs(UnmanagedType.U1)]
+    public static partial bool pf_jpsb_map_is_wall_at(HandleJpsbMap handleMap, int x, int y);
 
-    [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-    public static extern bool pf_jpsb_map_is_empty_at(HandleJpsbMap handleMap, int x, int y);
+    [LibraryImport(DLLNAME)]
+    [return: MarshalAs(UnmanagedType.U1)]
+    public static partial bool pf_jpsb_map_is_empty_at(HandleJpsbMap handleMap, int x, int y);
 
-    [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int pf_jpsb_map_get_width(HandleJpsbMap handleMap);
-    [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int pf_jpsb_map_get_height(HandleJpsbMap handleMap);
+    [LibraryImport(DLLNAME)]
+    public static partial int pf_jpsb_map_get_width(HandleJpsbMap handleMap);
+    [LibraryImport(DLLNAME)]
+    public static partial int pf_jpsb_map_get_height(HandleJpsbMap handleMap);
 
 
     // =========================================
     // pf_jpsb_pathfinder_
     // =========================================
-    [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-    public static extern E_ERRORCODE pf_jpsb_pathfinder_create(HandleJpsbMap handleMap, out HandlePathfinderJpsb outHandlePathfinder);
+    [LibraryImport(DLLNAME)]
+    public static partial E_ERRORCODE pf_jpsb_pathfinder_create(HandleJpsbMap handleMap, out HandlePathfinderJpsb outHandlePathfinder);
 
-    [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-    public static extern E_ERRORCODE pf_jpsb_pathfinder_destroy(IntPtr ptr);
+    [LibraryImport(DLLNAME)]
+    public static partial E_ERRORCODE pf_jpsb_pathfinder_destroy(IntPtr ptr);
 
 
     // =========================================
     // pf_pathfinder_
     // =========================================
-    [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int pf_pathfinder_find_path_with_smoothmethod(AHandlePathfinder handlePathfinder, int startX, int startY, int endX, int endY, E_SMOOTHMETHOD smoothMethod, ref int2 outPathBuf, int maxBufLen);
+    [LibraryImport(DLLNAME)]
+    public static partial int pf_pathfinder_find_path_with_smoothmethod(AHandlePathfinder handlePathfinder, int startX, int startY, int endX, int endY, E_SMOOTHMETHOD smoothMethod, ref int2 outPathBuf, int maxBufLen);
 
-    [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int pf_pathfinder_openlist_ensuretotalcapacity(AHandlePathfinder handlePathfinder, uint capacity);
+    [LibraryImport(DLLNAME)]
+    public static partial int pf_pathfinder_openlist_ensuretotalcapacity(AHandlePathfinder handlePathfinder, uint capacity);
 
-    [DllImport(DLLNAME, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int pf_pathfinder_pathbuffer_ensuretotalcapacity(AHandlePathfinder handlePathfinder, uint capacity);
+    [LibraryImport(DLLNAME)]
+    public static partial int pf_pathfinder_pathbuffer_ensuretotalcapacity(AHandlePathfinder handlePathfinder, uint capacity);
 
-    // [DllImport("native_lib", CallingConvention = CallingConvention.Cdecl)]
-    // private static extern void print_user_name([MarshalAs(UnmanagedType.LPUTF8Str)] string name);
+    // [LibraryImport("native_lib", CallingConvention = CallingConvention.Cdecl)]
+    // private static partial void print_user_name([MarshalAs(UnmanagedType.LPUTF8Str)] string name);
+    //    [LibraryImport(DLLNAME, StringMarshalling = StringMarshalling.Utf8)]
+    //    private static partial void print_user_name(string name);
 }
